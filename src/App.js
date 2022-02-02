2c8c0f87fa9d03f28f447ac143443f0f0b439934
Siri <siri.lerdsirisopon@gmail.com>
2022-02-02 10:11:50 +0100

    fixed custom hook useFetchCountries.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,13 @@
-import './App.css';
-import { useState } from 'react';
+//import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home/Home';
 import CountryDetails from './components/CountryDetails/CountryDetails';
-
 import useFetchCountries from './components/hooks/useFetchCountries';
+import './App.css';
 
 function App() {
-  /*
-  const [countries, setCountries] = useState([]);
-  const [filteredList, setFilteredList] = useState([]);
-
-  useEffect(() => {
-      fetch('https://restcountries.com/v3.1/all')
-          .then((response) => response.json())
-          .then((data) => {
-            setCountries(data);
-            setFilteredList(data);
-          })
-          .catch((error) => alert("Problem fetching data"));
-  }, []);
-  */
-
-  const [countries] = useFetchCountries();
-  const [filteredList, setFilteredList] = useState([]);
-  
-   /*
-  let result = countries;
-  setFilteredList(result);
-
-  This code above results in infinite re-render loop.
-  How to start filteredList with a full list of countries?
-  */
-
-  const searchFunc = (evt) => {
-    let result = countries.filter((country) => {
-      return (
-        country.name.common.toLowerCase().search(evt.target.value.toLowerCase()) !== -1
-      );
-    })
-
-    setFilteredList(result); 
-  }
+  const [filteredList, searchFunc] = useFetchCountries();
 
   return (
     <BrowserRouter>
--- src/components/hooks/useFetchCountries.js
+++ src/components/hooks/useFetchCountries.js
@@ -2,15 +2,27 @@ import { useState, useEffect } from 'react';
 
 export default function useFetchCountries() {
     const [countries, setCountries] = useState([]);
+    const [filteredList, setFilteredList] = useState([]);
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
             .then((response) => response.json())
             .then((data) => {
                 setCountries(data);
+                setFilteredList(data);
             })
             .catch((error) => alert("Problem fetching data"));
     }, []);
 
-    return [countries];
+    const searchFunc = (evt) => {
+        let result = countries.filter((country) => {
+          return (
+            country.name.common.toLowerCase().search(evt.target.value.toLowerCase()) !== -1
+          );
+        })
+    
+        setFilteredList(result); 
+      }
+
+    return [filteredList, searchFunc];
 }
